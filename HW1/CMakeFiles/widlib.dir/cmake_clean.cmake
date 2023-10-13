6ef88410e402eff59b5f9648ee694acc26c9cc35
Mário Hatalčík <xhatalc@fi.muni.cz>
2023-10-13 11:39:04 +0200

    HW1.1

---
+++ HW1/CMakeFiles/widlib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/widlib.dir/hw1.cpp.o"
+  "CMakeFiles/widlib.dir/hw1.cpp.o.d"
+  "libwidlib.pdb"
+  "libwidlib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/widlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
